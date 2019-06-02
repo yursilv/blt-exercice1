62ae79bdec5944cd3b0d5af1d7eddfdfef20e293
Yury Silvestrov-Henocq <yury.silvestrov-henocq@etu.unice.fr>
2019-06-02 17:23:02 +0200

    precision fix

--- exercice1.js
+++ exercice1.js
@@ -1,6 +1,19 @@
 function sum(form) {
-  const firstNumber = +form.firstNumber.value;
-  const secondNumber = +form.secondNumber.value;
-  const result = firstNumber + secondNumber;
-  alert("Результат:\n\n" + result);
+  const firstNumber = form.firstNumber.value;
+  const secondNumber = form.secondNumber.value;
+  let result = (+firstNumber) + (+secondNumber);
+
+  let firstNumberPrecision = 0;
+  let secondNumberPrecision = 0;
+
+  if (firstNumber.includes('.')) {
+  	firstNumberPrecision = firstNumber.split('.')[1].length;
+  }
+  if (secondNumber.includes('.')) {
+    secondNumberPrecision = secondNumber.split('.')[1].length;
+  }
+  
+  const maxPrecision = firstNumberPrecision > secondNumberPrecision ? firstNumberPrecision : secondNumberPrecision;
+
+  alert("Результат:\n\n" + result.toFixed(maxPrecision));
 }
